778fddeeff3ef86c58f6532898ce254a4757fda9
Larissa Novais <larinovais54@gmail.com>
2022-01-10 14:51:20 -0300

    TESTE

---
+++ Documents/aula introdutoria de React/aula-introdutoria-de-react/src/componentes/PrimeiroComponente.js
@@ -0,0 +1,12 @@
+import react from 'react'
+
+function PrimeiroComponente(){
+   return (
+       <div>
+       <h1> hello </h1> 
+       <h2>BEM VINDO AS AULAS</h2>
+       </div> 
+   )
+}
+
+export default PrimeiroComponente;
